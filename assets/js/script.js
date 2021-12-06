82d883d95061ad72456f7924b6f1ebd0ddef2337
RadwaAwed <dev.radwaawed@gmail.com>
2021-12-06 19:57:36 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  // navber
+  $(window).scroll(function () {
+    let wScroll = $(window).scrollTop();
+    if(wScroll >= 100){
+      $("nav").addClass("bgWidth");
+      $(".navbar-collapse").removeClass("show")
+    }else{
+      $("nav").removeClass("bgWidth")
+    }
+  });
+  $(".navbar-toggler").click(function () { 
+    $(this).toggleClass("close");
+  });
+  // calendar
+  let calendarDays = Array.from( document.querySelectorAll(".calendar-days .calendar-day-hover "));
+  calendarDays.forEach((button) =>{
+
+    button.addEventListener("click" , clicEvent);
+
+      function clicEvent(e){
+        let valueContent =  e.target.textContent;
+        alert( "Date " + valueContent);
+      }
+    })
+
+    
+function loder () {
+  document.querySelector(".loading-page").classList.add("fade-out");
+}
+function fadeOut (){
+  setInterval(loder ,3000)
+}
+window.onload = fadeOut()
+
+})
+
